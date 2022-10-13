fbf2f42615973693420a8e662272a13473d3ea0d
Kushnir Maxim <maxbasta2006@gmail.com>
2022-10-13 17:50:57 +0200

    light fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from 'src/app/header/header.component';
 
 import { FormsModule } from '@angular/forms';
 
