81ce1b2e7c399c79c28c3d6a38e3e20f3f993757
Kushnir Maxim <maxbasta2006@gmail.com>
2022-10-13 16:30:13 +0200

    header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { CurrencydataService } from './currencydata.service';
-// import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -43,56 +42,53 @@ export class AppComponent {
 
     this.currency.getCurrencyData(str).subscribe( data => {
       curNum === 1 ? this.firstCurrJson = JSON.stringify(data) : this.secondCurrJson = JSON.stringify(data);
-      curNum === 1 ? this.firstCurrJson = JSON.parse(this.firstCurrJson) : this.secondCurrJson = JSON.parse(this.secondCurrJson); 
-
-      curNum === 1 ? console.log(this.firstCurrJson) : console.log(this.secondCurrJson);
-      
+      curNum === 1 ? this.firstCurrJson = JSON.parse(this.firstCurrJson) : this.secondCurrJson = JSON.parse(this.secondCurrJson);       
     })
   }
 
-  // "USD"
-  // "UAH"
   onFirstInput() {
     if (this.firstCurrency === this.secondCurrency) {
       this.input2.setValue(this.input.value);
       this.input.setValue(this.input2.value);
-    } else {      
-      switch (this.secondCurrency) {
-        case "UAH":
-          this.input2.setValue(this.input.value * this.firstCurrJson.rates["UAH"]);
-          console.log(this.secondCurrency);
-          break;
-        case "EUR":
-          this.input2.setValue(this.input.value * this.firstCurrJson.rates["EUR"]); 
-          console.log(this.secondCurrency);
-          break;
-        case "USD":
-          this.input2.setValue(this.input.value * this.firstCurrJson.rates["USD"]); 
-          console.log(this.secondCurrency);          
-          break;
-      }
+    }       
+    
+    switch (this.secondCurrency.toLocaleLowerCase()) {
+      case "uah":
+        this.input2.setValue(this.input.value * this.firstCurrJson.rates["UAH"]);
+        console.log(this.secondCurrency);
+        break;
+      case "eur":
+        this.input2.setValue(this.input.value * this.firstCurrJson.rates["EUR"]); 
+        console.log(this.secondCurrency);
+        break;
+      case "usd":
+        this.input2.setValue(this.input.value * this.firstCurrJson.rates["USD"]); 
+        console.log(this.input.value);          
+        break;
     }
+    
   } 
 
   onSecondInput() {
     if (this.secondCurrency === this.firstCurrency) {
       this.input.setValue(this.input2.value);
       this.input2.setValue(this.input.value);
-    } else {      
-        switch (this.firstCurrency) {
-          case "UAH":
-            this.input.setValue(this.input2.value * this.secondCurrJson.rates["UAH"]);
-            break;
-          case "EUR":
-            this.input.setValue(this.input2.value * this.secondCurrJson.rates["EUR"]); 
-            console.log(this.secondCurrency);
-            break;
-          case "USD":
-            this.input.setValue(this.input2.value * this.secondCurrJson.rates["USD"]); 
-            console.log(this.secondCurrency);          
-            break;
-        }
-      }
+    } 
+        
+    switch (this.firstCurrency.toLocaleLowerCase()) {
+      case "uah":
+        this.input.setValue(this.input2.value * this.secondCurrJson.rates["UAH"]);
+        break;
+      case "eur":
+        this.input.setValue(this.input2.value * this.secondCurrJson.rates["EUR"]); 
+        console.log(this.secondCurrency);
+        break;
+      case "usd": 
+        this.input.setValue(this.input2.value * this.secondCurrJson.rates["USD"]); 
+        console.log(this.secondCurrency);          
+        break;
     }
+      
+  }
 }
 
